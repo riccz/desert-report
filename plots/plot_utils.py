cd9a109fc633542bd6c80aa524ae3aa1988551c9
Riccardo Zanol <ricc@zanol.eu>
2016-09-27 19:48:49 +0200

    Add function to run a simulation

--- plots/plot_utils.py
+++ plots/plot_utils.py
@@ -1,5 +1,18 @@
 import csv
 import numpy as np
+import os
+import subprocess
+
+def build_cmdline(scriptname, args):
+    args_str = list(map(lambda k: '--{}={}'.format(k, args[k]), args))
+    args_str.insert(0, 'ns')
+    args_str.insert(1, scriptname)
+    return args_str
+
+def merge_dicts(a, b):
+    m = a.copy()
+    m.update(b)
+    return m
 
 def read_csv_columns(filename):
     data = np.loadtxt(filename, delimiter=",",skiprows=1,unpack=True)
@@ -13,3 +26,14 @@ def read_csv_columns(filename):
         for i in range(0,len(first)):
             cols[first[i]] = data[i].tolist()
     return cols
+
+def run_ns(scriptname, csvname, fixed_params={}, run_params={}):
+    relative_fn = os.path.join("../ns/", csvname)
+    if os.path.exists(relative_fn):
+        os.remove(relative_fn)
+
+    for r in run_params:
+        args = {'csv_output': 1, 'csv_filename': csvname}
+        args.update(fixed_params)
+        args.update(r)
+        subprocess.check_call(build_cmdline(scriptname, args), cwd='../ns/')
